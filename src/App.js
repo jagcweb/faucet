51ee83be5fc52a34ec06d1ddb0a2a3e057e604e8
jagcweb <jagc.webdev@gmail.com>
2022-11-24 17:35:34 +0100

    Final commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { loadContract } from "./utils/load-contract";
 function App() {
   const [web3Api, setWeb3Api] = useState({
     provider: null,
+    isProviderLoaded: false,
     web3: null,
     contract: null,
   });
@@ -15,26 +16,37 @@ function App() {
   const [account, setAccount] = useState(null);
   const [shouldReload, reload] = useState(false);
 
+  const canConnectToContract = account && web3Api.contract;
+
   const reloadEffect = useCallback(() => reload(!shouldReload), [shouldReload]);
 
   const setAccountListener = (provider) => {
-    provider.on("accountsChanged", (accounts) => setAccount(accounts[0]));
+    provider.on("accountsChanged", (_) => window.location.reload());
+    provider.on("chainChanged", (_) => window.location.reload());
+
+    /*provider._jsonRpcConnection.events.on("notification", payload => {
+      const { method } = payload;
+
+      if (method === "metamask_unlockStateChange") {
+        setAccount(null);
+      }
+    });*/
   };
 
   useEffect(() => {
-    document.title = "Faucet";
     const loadProvider = async () => {
       const provider = await detectEthereumProvider();
-      const contract = await loadContract("Faucet", provider);
       if (provider) {
+        const contract = await loadContract("Faucet", provider);
         setAccountListener(provider);
         setWeb3Api({
           provider,
+          isProviderLoaded: true,
           web3: new Web3(provider),
           contract,
         });
       } else {
-        // if the provider is not detected, detectEthereumProvider resolves to null
+        setWeb3Api({ ...web3Api, isProviderLoaded: true });
         console.log("Please install MetaMask!");
       }
     };
@@ -46,7 +58,6 @@ function App() {
     const loadBalance = async () => {
       const { contract, web3 } = web3Api;
       const balance = await web3.eth.getBalance(contract.address);
-      console.log(balance);
       setBalance(web3.utils.fromWei(balance, "ether"));
     };
 
@@ -87,35 +98,69 @@ function App() {
     <>
       <div className="faucet-wrapper">
         <div className="faucet is-flex is-flex-direction-column is-align-items-center">
-          <div className="mt-4 is-flex is-flex-direction-row is-align-items-center">
-            <span>
-              <strong>Account: </strong>
-            </span>
-            {account ? (
-              <div className="ml-2">{account}</div>
-            ) : (
-              <button
-                className="button ml-2"
-                onClick={() =>
-                  web3Api.provider.request({ method: "eth_requestAccounts" })
-                }
-              >
-                Connect Metamask
-              </button>
-            )}
-          </div>
-
-          <div className="mt-4 balance-view is-size-2">
-            Current Contract Balance: <strong>{balance}</strong> ETH
-          </div>
-          <div className="is-flex is-flex-direction-row is-align-items-center">
-            <button className="button is-primary mr-2" onClick={addFunds}>
-              Donate 1ETH
-            </button>
-            <button className="button is-link" onClick={withdrawFunds}>
-              Withdraw
-            </button>
-          </div>
+          {web3Api.isProviderLoaded ? (
+            <>
+              <div className="mt-4 is-flex is-flex-direction-row is-align-items-center">
+                <span>
+                  <strong>Account: </strong>
+                </span>
+                {account ? (
+                  <div className="ml-2">{account}</div>
+                ) : !web3Api.provider ? (
+                  <>
+                    <div className="notification is-warning is-rounded">
+                      Wallet is not detected!
+                      <a
+                        className="ml-2"
+                        target="_blank"
+                        href="https://metamask.io/download/"
+                        rel="noreferrer"
+                      >
+                        Install Metamask
+                      </a>
+                    </div>
+                  </>
+                ) : (
+                  <button
+                    className="button ml-2"
+                    onClick={() =>
+                      web3Api.provider.request({
+                        method: "eth_requestAccounts",
+                      })
+                    }
+                  >
+                    Connect Metamask
+                  </button>
+                )}
+              </div>
+              <div className="mt-4 balance-view is-size-2">
+                Current Contract Balance: <strong>{balance}</strong> ETH
+              </div>
+              {!canConnectToContract && account ? (
+                <i className="is-block mb-2">
+                  Wrong network. Connect to Ganache.
+                </i>
+              ): <></>}
+              <div className="is-flex is-flex-direction-row is-align-items-center">
+                <button
+                  className="button is-primary mr-2"
+                  onClick={addFunds}
+                  disabled={!canConnectToContract}
+                >
+                  Donate 1ETH
+                </button>
+                <button
+                  className="button is-link"
+                  onClick={withdrawFunds}
+                  disabled={!canConnectToContract}
+                >
+                  Withdraw
+                </button>
+              </div>
+            </>
+          ) : (
+            <span>Loading web3...</span>
+          )}
         </div>
       </div>
     </>
